1e863619f9389f070601e5f1d6a9f80db662fc06
Andrery Eroshkin <eroshkin.andrey@gmail.com>
2020-11-08 17:17:10 +0300

    change "import from" on "import"

--- re_field.py
+++ re_field.py
@@ -1,12 +1,12 @@
 import random
-from tempfile import mkstemp
-from shutil import move, copymode
+import tempfile
+import shutil
 import os
 
 def replace(file_path, tgt_fld: str, a=1000, b=10_000):
     is_changed = False
     #Create temp file
-    fh, abs_path = mkstemp()
+    fh, abs_path = tempfile.mkstemp()
     with os.fdopen(fh, 'w') as new_file:
         with open(file_path) as old_file:
             for line in old_file:
@@ -19,11 +19,11 @@ def replace(file_path, tgt_fld: str, a=1000, b=10_000):
     else:
         print(f'Field "{tgt_fld}" in {file_path} not found.')
     #Copy the file permissions from the old file to the new file
-    copymode(file_path, abs_path)
+    shutil.copymode(file_path, abs_path)
     #Remove original file
     os.remove(file_path)
     #Move new file
-    move(abs_path, file_path)
+    shutil.move(abs_path, file_path)
 
 def get_files_paths (basepath: str):
     for fname in os.listdir(PATH_DIR):
@@ -46,5 +46,3 @@ if __name__ == '__main__':
         except Exception as e:
             print(f'Error while processing file {file}.')
             print(e)
-
-
